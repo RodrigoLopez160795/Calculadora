8251516db0d79556c02044e3d1dd2fccff90ac91
Rodrigo Lopez Alvarado <rodrigo.lopez.160795@hotmail.com>
2021-03-27 21:09:37 -0600

    Primer commit

---
+++ src/calculadoragrafica/Calculadoragrafica.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoragrafica;
+
+/**
+ *
+ * @author gigon
+ */
+public class Calculadoragrafica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calcugrafica objeto=new Calcugrafica();
+        objeto.setVisible(true);
+    }
+    
+}
